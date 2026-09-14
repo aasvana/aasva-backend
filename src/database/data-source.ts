da092154907ceff260a00a91da17683ce5e3cb0e
Aquib Shahbaz <justaquib@Aquibs-MacBook-Air.local>
2026-09-14 16:44:15 +0530

    Fix the database connection resolve

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -13,11 +13,18 @@ import { CompanySetting } from '../company/entities/company-setting.entity';
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: process.env.DB_HOST ?? 'localhost',
-  port: Number(process.env.DB_PORT ?? 5432),
-  username: process.env.DB_USERNAME ?? 'postgres',
-  password: process.env.DB_PASSWORD ?? 'postgres',
-  database: process.env.DB_DATABASE ?? 'aasvaDB',
+  ...(process.env.DATABASE_URL
+    ? {
+        url: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false },
+      }
+    : {
+        host: process.env.DB_HOST ?? 'localhost',
+        port: Number(process.env.DB_PORT ?? 5432),
+        username: process.env.DB_USERNAME ?? 'postgres',
+        password: process.env.DB_PASSWORD ?? 'postgres',
+        database: process.env.DB_DATABASE ?? 'aasvaDB',
+      }),
   entities: [
     User,
     ProfileType,
