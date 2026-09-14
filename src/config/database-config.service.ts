2f3c1a7922db35b0695e21793068627b4f350d2e
Aquib Shahbaz <justaquib@Aquibs-MacBook-Air.local>
2026-09-14 18:29:20 +0530

    Fix the database connection resolve

--- src/config/database-config.service.ts
+++ src/config/database-config.service.ts
@@ -11,11 +11,18 @@ export class DatabaseConfigService implements TypeOrmOptionsFactory {
 
     return {
       type: 'postgres',
-      host: this.config.get<string>('DB_HOST', 'localhost'),
-      port: this.config.get<number>('DB_PORT', 5432),
-      username: this.config.get<string>('DB_USERNAME', 'postgres'),
-      password: this.config.get<string>('DB_PASSWORD', 'postgres'),
-      database: this.config.get<string>('DB_DATABASE', 'aasvaDB'),
+      ...(process.env.DATABASE_URL
+        ? {
+            url: process.env.DATABASE_URL,
+            ssl: { rejectUnauthorized: false },
+          }
+        : {
+            host: process.env.DB_HOST ?? 'localhost',
+            port: Number(process.env.DB_PORT ?? 5432),
+            username: process.env.DB_USERNAME ?? 'postgres',
+            password: process.env.DB_PASSWORD ?? 'postgres',
+            database: process.env.DB_DATABASE ?? 'aasvaDB',
+          }),
       autoLoadEntities: true,
       synchronize: false,
       logging: isProduction
